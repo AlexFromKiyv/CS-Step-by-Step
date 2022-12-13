daa8bce7a343556ff15d7d2f4abcd60d03334a15
AlexFromKiyv <bisoftkievua@gmail.com>
2022-12-13 12:14:45 +0200

    Add GetParametersInApp

---
+++ 02 Простий консольного проект. Класси Consol та Environment/SimpleConsoleSolution/GetParametersInApp/Program.cs
@@ -0,0 +1,9 @@
+﻿int length = args.Length;
+string[] appArgs = Environment.GetCommandLineArgs();
+
+Console.WriteLine($"The number of parameters:{length}");
+for (int i = 0; i < length; i++)
+{
+    Console.WriteLine($"Prameter {i}:" + args[i]);
+}
+
