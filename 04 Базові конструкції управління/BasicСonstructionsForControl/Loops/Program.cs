4834554336accd379abf3a6d0f6cde3485657706
AlexFromKiyv <bisoftkievua@gmail.com>
2022-12-31 19:57:49 +0200

    UsingForeach()

--- 04 Базові конструкції управління/BasicСonstructionsForControl/Loops/Program.cs
+++ 04 Базові конструкції управління/BasicСonstructionsForControl/Loops/Program.cs
@@ -1,9 +1,21 @@
-﻿UsingFor();
+﻿//UsingFor();
 static void UsingFor()
 {
 	int length = 10;
 	for (int i = 0; i < length; i++)
 	{
 		Console.WriteLine(i);
+	}
+}
+
+UsingForeach();
+
+static void UsingForeach()
+{
+	string[] myTools = { "Апценьки", "Молоток", "Вивертка" };
+
+	foreach(string tool in myTools)
+	{
+		Console.WriteLine(tool);
 	}
 }
