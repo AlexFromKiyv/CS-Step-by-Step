8c343ca18a36b49ddad65c030bbeba0418b87d4d
AlexFromKiyv <bisoftkievua@gmail.com>
2023-01-09 11:45:29 +0200

    Add folder for 05 UsingSimpleArray

---
+++ 05 Базові конструкції данних/BasicConstructionsForData/Array/Program.cs
@@ -0,0 +1,23 @@
+﻿UsingSimpleArray_1();
+
+static void UsingSimpleArray_1()
+{
+    int[] myIntArray = new int[3]; // declaring
+
+    myIntArray[0] = 1;
+    myIntArray[1] = 2;
+    myIntArray[2] = 3;
+
+    for (int i = 0; i < 3; i++)
+    {
+        Console.WriteLine(myIntArray[i]);
+    }
+
+    //myIntArray[3] = 4; //do not work. Out of index
+    int[] myNewArray = new int[10];
+
+    foreach (int item in myNewArray)
+    {
+        Console.WriteLine(item);
+    }
+}
