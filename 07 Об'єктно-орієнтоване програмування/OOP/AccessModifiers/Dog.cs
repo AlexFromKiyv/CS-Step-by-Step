4ae3dce3bf8280539ec743bcf320aea8c74c1d2a
AlexFromKiyv <bisoftkievua@gmail.com>
2023-03-03 12:25:54 +0200

    Dog

---
+++ 07 Об'єктно-орієнтоване програмування/OOP/AccessModifiers/Dog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    public class Dog
+    {
+        public string Breed;
+
+        public Dog(string breed)
+        {
+            Breed = breed;
+        }
+    }
+}
---
+++ 07 Об'єктно-орієнтоване програмування/OOP/AccessModifiers/Program.cs
@@ -0,0 +1,10 @@
+﻿using AccessModifiers;
+
+//LivingOrganism livingOrganism = new(); // LivingOrganism.LivingOrganism() inaccesible
+
+Animal animal = new();
+
+// animal.weight //no access
+
+Dog snupy = new("mutt");
+Console.WriteLine(snupy.Breed);
