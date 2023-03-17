ecc4c74531c6608f0e91dc78b0ecbf5c96cba367
AlexFromKiyv <bisoftkievua@gmail.com>
2023-03-17 11:56:05 +0200

    Class Diagrams

---
+++ 07 Об'єктно-орієнтоване програмування/OOP/BasicInheritance/Bus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BasicInheritance
+{
+    internal class Bus : Car
+    {
+        public int NumberOfSeat
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
