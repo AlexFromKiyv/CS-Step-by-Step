0781cea6230181f0f56790f4a6e1ef5c253a42cd
AlexFromKiyv <bisoftkievua@gmail.com>
2023-04-06 20:04:58 +0300

    ExplorationTryCatch

--- 08 Обробка винятків/Exeptions/BaseClassExeption/Program.cs
+++ 08 Обробка винятків/Exeptions/BaseClassExeption/Program.cs
@@ -12,7 +12,7 @@ void ExplorationTheOccurationOfAnException()
 
 }
 
-ExplorationThrow();
+//ExplorationThrow();
 void ExplorationThrow()
 {
     Car_v2 car = new("Nissan Leaf", 35);
@@ -21,4 +21,33 @@ void ExplorationThrow()
     {
         car.Accelerate(20);
     }
+}
+
+ExplorationTryCatch();
+void ExplorationTryCatch()
+{
+    Car_v2 car = new("Nissan Leaf", 35);
+
+    Console.WriteLine("---The begin of try---\n");
+    try
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            car.Accelerate(20);
+        }
+
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(  );
+
+        string stringForShow = "\n" +
+            $"Attention! Problem occured!\n\n" +
+            $" Method: {e.TargetSite}\n" +
+            $" Message: {e.Message}\n" +
+            $" Source: {e.Source}\n";
+
+        Console.WriteLine(stringForShow);
+    }
+    Console.WriteLine("---The end of try---");
 }
