f0baef5882e99bf035ec466d029f39f2b2851844
AlexFromKiyv <bisoftkievua@gmail.com>
2023-05-07 15:31:19 +0300

    ExplorationNamedEnumerator

---
+++ 07 Об'єктно-орієнтоване програмування/13 Корисні інтерфейси .Net/Useful_Interfaces/IEnumerable_IEnumerator/Program.cs
@@ -0,0 +1,112 @@
+﻿
+using IEnumerable_IEnumerator;
+
+//ArrayIsEnumerable();
+void ArrayIsEnumerable()
+{
+    int[] temperatures = { 4, 6, 8 };
+
+    foreach (int item in temperatures)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+//IEnumerableFromArray_1();
+void IEnumerableFromArray_1()
+{
+    Garage garage = new();
+
+    foreach (var item in garage)
+    {
+        Console.WriteLine($"{((Car)item).Name}");
+    }
+
+    Console.WriteLine();
+
+    var ie = garage.GetEnumerator();
+
+    Console.WriteLine($"The method MoveNext returned : {ie.MoveNext()}");
+    Console.Write($"{((Car)ie.Current).Name}\t");
+    Console.WriteLine($"The method Current returned : {ie.Current}\t");
+
+    Console.WriteLine($"The method MoveNext returned : {ie.MoveNext()}\t{((Car)ie.Current).Name} ");
+    Console.WriteLine($"The method MoveNext returned : {ie.MoveNext()}\t{((Car)ie.Current).Name} ");
+    Console.WriteLine($"The method MoveNext returned : {ie.MoveNext()}\t{((Car)ie.Current).Name} ");
+    Console.WriteLine($"The method MoveNext returned : {ie.MoveNext()}");
+    ie.Reset();
+    Console.WriteLine($"Reset");
+    Console.WriteLine($"The method MoveNext returned : {ie.MoveNext()}\t{((Car)ie.Current).Name} ");
+    Console.WriteLine($"The method MoveNext returned : {ie.MoveNext()}\t{((Car)ie.Current).Name} ");
+    Console.WriteLine($"The method MoveNext returned : {ie.MoveNext()}\t{((Car)ie.Current).Name} ");
+    Console.WriteLine($"The method MoveNext returned : {ie.MoveNext()}\t{((Car)ie.Current).Name} ");
+    Console.WriteLine($"The method MoveNext returned : {ie.MoveNext()}\t{((Car)ie.Current).Name} ");
+}
+
+
+//IEnumerableFromArray_2();
+void IEnumerableFromArray_2()
+{
+    Garage_v1 garage = new();
+
+    //var enumerator = garage.GetEnumerator(); //class cannot contain definition GetEnumerator  
+
+    foreach (Car item in garage)
+    {
+        Console.WriteLine($"{item.Name} {item.MaxSpeed}");
+    }
+}
+
+//ExamineIEnumerableWithYield();
+void ExamineIEnumerableWithYield()
+{
+    Garage_v2 garage = new();
+
+    foreach (Car item in garage)
+    {
+        Console.WriteLine($"{item.Name} {item.MaxSpeed}");
+    }
+
+}
+
+//ExplorationGetEnumerator();
+void ExplorationGetEnumerator()
+{
+    Garage_v3 garage = new();
+
+    var enumerator = garage.GetEnumerator();
+
+    Console.WriteLine("After called GetEnumerator");
+
+    enumerator.MoveNext();
+}
+
+//ExplorationGetEnumeratorWithFunction();
+void ExplorationGetEnumeratorWithFunction()
+{
+    Garage_v4 garage = new();
+
+    var enumerator = garage.GetEnumerator();
+
+    Console.WriteLine("After called GetEnumerator");
+
+    enumerator.MoveNext();
+}
+
+
+
+ExplorationNamedEnumerator();
+void ExplorationNamedEnumerator()
+{
+    Garage_v5 garage = new();
+
+    foreach (Car item in garage.GetTheCars(true))
+    {
+        Console.WriteLine($"{item.Name} {item.MaxSpeed}");
+    }
+
+    foreach (Car item in garage.GetTheCars())
+    {
+        Console.WriteLine($"{item.Name} {item.MaxSpeed}");
+    }
+}
---
+++ 07 Об'єктно-орієнтоване програмування/13 Корисні інтерфейси .Net/Useful_Interfaces/IEnumerable_IEnumerator/Types_v1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerable_IEnumerator
+{
+    class Car
+    {
+        public string Name { get; set; } = "";
+        public int MaxSpeed { get; set; }
+
+        public Car(string name, int maxSpeed)
+        {
+            Name = name;
+            MaxSpeed = maxSpeed;
+        }
+
+        public Car()
+        {
+        }
+    }
+
+    class Garage : IEnumerable
+    {
+        private Car[] carArray = new Car[4];
+        public Garage()
+        {
+            carArray[0] = new Car("VW Golf 1", 145);
+            carArray[1] = new Car("VW Golf 2", 167);
+            carArray[2] = new Car("VW Golf 3", 168);
+            carArray[3] = new Car("VW Golf 4", 192);
+        }
+
+        public IEnumerator GetEnumerator() =>  carArray.GetEnumerator();
+    }
+
+    class Garage_v1 : IEnumerable
+    {
+        private Car[] carArray = new Car[4];
+        public Garage_v1()
+        {
+            carArray[0] = new Car("VW Golf 1", 145);
+            carArray[1] = new Car("VW Golf 2", 167);
+            carArray[2] = new Car("VW Golf 3", 168);
+            carArray[3] = new Car("VW Golf 4", 192);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => carArray.GetEnumerator();
+    }
+
+    class Garage_v2 : IEnumerable
+    {
+        private Car[] carArray = new Car[4];
+        public Garage_v2()
+        {
+            carArray[0] = new Car("VW Golf 1", 145);
+            carArray[1] = new Car("VW Golf 2", 167);
+            carArray[2] = new Car("VW Golf 3", 168);
+            carArray[3] = new Car("VW Golf 4", 192);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            yield return carArray[0];
+            yield return carArray[1];
+            yield return carArray[2];
+            yield return carArray[3];
+
+            foreach (Car car in carArray)
+            {
+                yield return car; 
+            }
+
+            yield return new Car("VW Polo",160);
+            yield return new Car("VW Passat",180);
+
+        }
+    }
+
+    class Garage_v3 : IEnumerable
+    {
+        private Car[] carArray = new Car[4];
+        public Garage_v3()
+        {
+            carArray[0] = new Car("VW Golf 1", 145);
+            carArray[1] = new Car("VW Golf 2", 167);
+            carArray[2] = new Car("VW Golf 3", 168);
+            carArray[3] = new Car("VW Golf 4", 192);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            Console.WriteLine("Without MoveNext, it does not work.");
+            yield return carArray[0];
+            yield return carArray[1];
+        }
+    }
+
+    class Garage_v4 : IEnumerable
+    {
+        private Car[] carArray = new Car[4];
+        public Garage_v4()
+        {
+            carArray[0] = new Car("VW Golf 1", 145);
+            carArray[1] = new Car("VW Golf 2", 167);
+            carArray[2] = new Car("VW Golf 3", 168);
+            carArray[3] = new Car("VW Golf 4", 192);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            Console.WriteLine("Work in GetEnumerator (For example connect to DB)");
+
+            return ActualImplementation();
+
+            IEnumerator ActualImplementation()
+            {
+                yield return carArray[0];
+                yield return carArray[1];
+                yield return carArray[2];
+            }
+        }
+    }
+
+    class Garage_v5 
+    {
+        private Car[] carArray = new Car[4];
+        public Garage_v5()
+        {
+            carArray[0] = new Car("VW Golf 1", 145);
+            carArray[1] = new Car("VW Golf 2", 167);
+            carArray[2] = new Car("VW Golf 3", 168);
+            carArray[3] = new Car("VW Golf 4", 192);
+        }
+
+        public IEnumerable GetTheCars(bool returnReversed = false)
+        {
+
+            return ActualImplementation();
+
+            IEnumerable ActualImplementation()
+            {
+                if (returnReversed)
+                {
+                    for (int i = carArray.Length - 1; i >= 0; i--)
+                    {
+                        yield return carArray[i];
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < carArray.Length; i++)
+                    {
+                        yield return carArray[i];
+                    }
+                }
+            }
+        }
+    }
+
+}
