e27e0deceb0fe664a51e307b43eec8ee3466c641
AlexFromKiyv <bisoftkievua@gmail.com>
2023-05-13 12:03:00 +0300

    ExplorationClassObjectReference

---
+++ 07 Об'єктно-орієнтоване програмування/14 Цикл житя об'екта/ObjectLifetime/ObjectLifetime/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using ObjectLifetime;
+
+
+ExplorationClassObjectReference();
+void ExplorationClassObjectReference()
+{
+    int max = 115;    
+
+    Car myCar = new Car("Volkswagen Käfer", max, 30);
+
+    Console.WriteLine(myCar.ToString());
+}
