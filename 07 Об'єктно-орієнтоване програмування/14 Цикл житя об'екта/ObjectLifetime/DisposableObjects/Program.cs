b261e2f22c94c50d45becf3c16d0254899c80d4d
AlexFromKiyv <bisoftkievua@gmail.com>
2023-05-26 10:29:38 +0300

    UsingDisposableObjects

---
+++ 07 Об'єктно-орієнтоване програмування/14 Цикл житя об'екта/ObjectLifetime/DisposableObjects/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using DisposableObjects;
+UsingDisposableObjects();
+void UsingDisposableObjects()
+{
+    MyResourceWrapper myResourceWrapper = new MyResourceWrapper();
+
+    // Use object.
+    myResourceWrapper.Resource = "Here we are using unmanaged resource...";
+    Console.WriteLine(myResourceWrapper.Resource);
+
+    // Clean up.
+    if (myResourceWrapper is IDisposable)
+    {
+        myResourceWrapper.Dispose();
+    }
+
+}
