db6f3426383ad4ef10284af78c2fda76757ba3d7
AlexFromKiyv <bisoftkievua@gmail.com>
2023-06-06 14:22:06 +0300

    UsingMyBusLibrary

---
+++ 09 Бібліотеки класів, пакети, простори імен/Libraries/MyBusLibrary/Bus.cs
@@ -0,0 +1,28 @@
+﻿namespace MyBusLibrary;
+
+public class Bus
+{
+    public string Manufacturer { get; set; }
+    public string Model { get; set; }
+    public int? Year { get; set; }
+
+    public Bus(string manufacturer, string model, int? year)
+    {
+        Manufacturer = manufacturer;
+        Model = model;
+        Year = year;
+    }
+    public Bus(string manufacturer, string model)
+    {
+        Manufacturer = manufacturer;
+        Model = model;
+    }
+
+    public Bus()
+    {
+        Manufacturer = "Undefined";
+        Model = "Undefined";
+    }
+
+    public void ToConsole() => Console.WriteLine($"{Manufacturer}\t{Model}\t{Year}");
+}
