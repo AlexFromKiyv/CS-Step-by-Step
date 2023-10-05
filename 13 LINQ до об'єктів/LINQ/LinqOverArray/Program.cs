999aa69e130046a1dd04f94e3803f8b3eee70647
AlexFromKiyv <bisoftkievua@gmail.com>
2023-10-05 12:40:16 +0300

    Застосування LINQ до массивів

--- 13 LINQ до об'єктів/LINQ/LinqOverArray/Program.cs
+++ 13 LINQ до об'єктів/LINQ/LinqOverArray/Program.cs
@@ -1,4 +1,27 @@
-﻿void QuryesOverStrings()
+﻿void QuryesOverStringsArray()
 {
+    string[] games =
+    {
+        "Morrowind",
+        "Uncharted 2",
+        "Fallout 3",
+        "Daxter",
+        "System Shock 2"
+    };
 
+    // Build a query expression to find the items in the array
+    // that have an embedded space.
+
+    IEnumerable<string> longNames = 
+        from ng in games
+        where ng.Contains(" ")
+        orderby ng
+        select ng;
+
+    foreach (var item in longNames)
+    {
+        Console.WriteLine(item);
+    }
 }
+
+QuryesOverStringsArray();
