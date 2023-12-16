376bba3f1c2da03306332b423bef5c3a4bf46737
AlexFromKiyv <bisoftkievua@gmail.com>
2023-12-16 13:38:16 +0200

    System.Threading

---
+++ 15 Багатопотокове, паралельне та асинхронне програмування/01 Простір імен System.Threading/ThreadingNamespace/ThreadingNamespace/Program.cs
@@ -0,0 +1,22 @@
+﻿static void ExtractExecutingThread()
+{
+    Thread thread = Thread.CurrentThread;
+    Console.WriteLine(thread.ManagedThreadId);
+
+}
+//ExtractExecutingThread();
+
+static void ExtractAppDomainHostingThread()
+{
+    // Obtain the AppDomain hosting the current thread.
+    AppDomain appDomain = Thread.GetDomain();
+    Console.WriteLine(appDomain.FriendlyName);
+}
+//ExtractAppDomainHostingThread();
+
+static void ExtractCurrentThreadExecutionContext()
+{
+    ExecutionContext executionContext = Thread.CurrentThread.ExecutionContext;
+    Console.WriteLine(executionContext);
+}
+//ExtractCurrentThreadExecutionContext();
