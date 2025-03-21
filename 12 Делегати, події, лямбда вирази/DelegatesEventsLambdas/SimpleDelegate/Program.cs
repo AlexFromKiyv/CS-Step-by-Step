d3142840c067390a61432dff345c688691b96076
AlexFromKiyv <bisoftkievua@gmail.com>
2025-03-21 09:06:21 +0200

    Change 12

---
+++ 12 Делегати, події, лямбда вирази/DelegatesEventsLambdas/SimpleDelegate/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using SimpleDelegate;
+
+void UsingDelegate()
+{
+    // Create a BinaryOp delegate object that
+    // 'points to' SimpleMath.Add().
+    BinaryOp myAdd = new BinaryOp(SimpleMath.Add);
+
+    // Invoke Add() method indirectly using delegate object.
+    Console.WriteLine(myAdd(10,10));
+    Console.WriteLine(myAdd.Invoke(10,20));
+
+    // Compile Error
+    //BinaryOp sQ = new BinaryOp(SimpleMath.SquareNumber);
+}
+UsingDelegate();
+
+
+// Additional type definitions must be placed at the end of the
+// top-level statements
+
+// This delegate can point to any method,
+// taking two integers and returning an integer.
+public delegate int BinaryOp(int x, int y);
---
+++ 12 Делегати, події, лямбда вирази/DelegatesEventsLambdas/SimpleDelegate/SimpleMath.cs
@@ -0,0 +1,8 @@
+﻿namespace SimpleDelegate;
+
+class SimpleMath
+{
+    public static int Add(int a, int b) => a + b;
+    public static int Subtract(int a, int b) => a - b;
+    public static int SquareNumber(int a) => a * a;
+}
