ff4120e155994af92e4504254f63ade8b01f1d4f
AlexFromKiyv <bisoftkievua@gmail.com>
2025-04-19 10:59:57 +0300

    Chapter 21 4 Resources

---
+++ 21 WPF/04 Ресурси WPF, анімація, стилі та шаблони/ObjectResourcesApp/ObjectResourcesApp/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ObjectResourcesApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            // Get the brush and make a change.
+            var b = (RadialGradientBrush)Resources["MyBrush"];
+            b.GradientStops[1] = new GradientStop(Colors.WhiteSmoke, 0.0);
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            // Put a totally new brush into the myBrush slot.
+            Resources["MyBrush"] = new SolidColorBrush(Colors.Red);
+        }
+    }
+}
