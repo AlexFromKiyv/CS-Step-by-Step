76fa2ac3ec4dfe9d154892212567806bc08331cd
AlexFromKiyv <bisoftkievua@gmail.com>
2025-05-13 12:06:34 +0300

    Chapter 14

---
+++ 14 Процеси, домени програм і контексти завантаження/DefaultAppDomainApp/DefaultAppDomainApp/Program.cs
@@ -0,0 +1,70 @@
+﻿using ClassLibrary1;
+using System.Reflection;
+using System.Runtime.Loader;
+
+static void DisplayDefaulAppDomainStats()
+{
+    // Get access to the AppDomain for the current thread.
+    AppDomain defaultAD = AppDomain.CurrentDomain;
+
+    // Print out various stats about this domain.
+    Console.WriteLine($"Name of this domain: {defaultAD.FriendlyName}");
+    Console.WriteLine($"ID of domain in this process: {defaultAD.Id}");
+    Console.WriteLine($"Is this the default domain?: {defaultAD.IsDefaultAppDomain()}");
+    Console.WriteLine($"Base directory of this domain: {defaultAD.BaseDirectory}");
+    Console.WriteLine($"Setup Information for this domain:");
+    Console.WriteLine($"\tApplication Base: {defaultAD.SetupInformation.ApplicationBase}");
+    Console.WriteLine($"\tTarget Framework: {defaultAD.SetupInformation.TargetFrameworkName}");
+}
+//DisplayDefaulAppDomainStats();
+
+static void ListAllAssembliesInAppDomain()
+{
+    // Get access to the AppDomain for the current thread.
+    AppDomain defaultAD = AppDomain.CurrentDomain;
+
+    // Now get all loaded assemblies in the default AppDomain.
+    //var loadedAssemblies = defaultAD.GetAssemblies();
+    var loadedAssemblies = defaultAD.GetAssemblies().OrderBy(x => x.GetName().Name);
+    
+    Console.WriteLine($"***** Here are the assemblies loaded in {defaultAD.FriendlyName} *****\n");
+    foreach (Assembly a in loadedAssemblies)
+    {
+        Console.WriteLine($"\t{a.GetName().Name}:{a.GetName().Version}");
+    }
+}
+//ListAllAssembliesInAppDomain();
+
+static void LoadAdditionalAssembliesDifferentContexts()
+{
+    string? path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ClassLibrary1.dll");
+    AssemblyLoadContext lc1 = new AssemblyLoadContext("NewContext1", false);
+    Assembly assembly1 = lc1.LoadFromAssemblyPath(path);
+    object? car1 = assembly1.CreateInstance("ClassLibrary1.Car");
+    AssemblyLoadContext lc2 = new AssemblyLoadContext("NewContext2", false);
+    Assembly assembly2 = lc2.LoadFromAssemblyPath(path);
+    object? car2 = assembly2.CreateInstance("ClassLibrary1.Car");
+    Console.WriteLine("*** Loading Additional Assemblies in Different Contexts ***");
+    Console.WriteLine($"Assembly1.Equals(Assembly2) {assembly1.Equals(assembly2)}");
+    Console.WriteLine($"Assembly1 == Assembly2 {assembly1 == assembly2}");
+    Console.WriteLine($"Class1.Equals(Class2) {car1.Equals(car2)}");
+    Console.WriteLine($"Class1 == Class2 {car1 == car2}");
+}
+//LoadAdditionalAssembliesDifferentContexts();
+
+static void LoadAdditionalAssembliesSameContext()
+{
+    var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ClassLibrary1.dll");
+    AssemblyLoadContext lc1 = new AssemblyLoadContext(null, false);
+    var cl1 = lc1.LoadFromAssemblyPath(path);
+    var c1 = cl1.CreateInstance("ClassLibrary1.Car");
+    var cl2 = lc1.LoadFromAssemblyPath(path);
+    var c2 = cl2.CreateInstance("ClassLibrary1.Car");
+    Console.WriteLine("*** Loading Additional Assemblies in Same Context ***");
+    Console.WriteLine($"Assembly1.Equals(Assembly2) {cl1.Equals(cl2)}");
+    Console.WriteLine($"Assembly1 == Assembly2 {cl1 == cl2}");
+    Console.WriteLine($"Class1.Equals(Class2) {c1.Equals(c2)}");
+    Console.WriteLine($"Class1 == Class2 {c1 == c2}");
+}
+LoadAdditionalAssembliesSameContext();
+
---
+++ 14 Процеси, домени програм і контексти завантаження/ProcessManipulator/ProcessManipulator/Program.cs
@@ -0,0 +1,136 @@
+﻿using System.Diagnostics;
+
+static void ListAllRunningProcesses()
+{
+    // Get all the processes on the local machine, ordered by
+    // PID.
+    var runningProcs = from proc in Process.GetProcesses(".") 
+                       orderby proc.Id 
+                       select proc;
+
+    // Print out PID and name of each process.
+    foreach (var p in runningProcs)
+    {
+        string info = $"PID: {p.Id}\tName: {p.ProcessName}";
+        Console.WriteLine(info);
+    }
+    Console.WriteLine("************************************\n");
+}
+//ListAllRunningProcesses();
+
+static void GetSpecificProcess()
+{
+    // Get all the processes on the local machine, ordered by
+    // PID.
+    var runningProcs = from proc in Process.GetProcesses(".")
+                       orderby proc.Id
+                       select proc.Id;
+
+    int id = runningProcs.Max();
+    Process process = Process.GetProcessById(id);
+
+    Console.WriteLine(process.Id+"\t"+process.ProcessName);
+}
+//GetSpecificProcess();
+
+
+
+static void EnumThreadsForProcess(Process theProc)
+{
+
+    // List out stats for each thread in the specified process.
+    Console.WriteLine($"Here are the threads used by:{theProc.Id} {theProc.ProcessName}");
+    ProcessThreadCollection theThreads = theProc.Threads;
+
+    foreach (ProcessThread pt in theThreads)
+    {
+        string info =
+            $"-> Thread ID: {pt.Id}\tStart Time: {pt.StartTime.ToShortTimeString()}\tPriority: {pt.PriorityLevel}";
+        Console.WriteLine(info);
+    }
+    Console.WriteLine("************************************\n");
+}
+
+static void EnumThreads()
+{
+    var runningProcs = from proc in Process.GetProcesses(".")
+                       where proc.ProcessName == "chrome"
+                       select proc;
+
+    // Print out PID and name of each process.
+    foreach (var p in runningProcs)
+    {
+        EnumThreadsForProcess(p);
+    }
+}
+//EnumThreads();
+
+static void EnumModulesForProcess(Process theProc)
+{
+
+    Console.WriteLine($"Here are the loaded modules for:{theProc.Id} {theProc.ProcessName}");
+    ProcessModuleCollection theMods = theProc.Modules;
+    foreach (ProcessModule pm in theMods)
+    {
+        string info = $"-> Mod Name: {pm.ModuleName}";
+        Console.WriteLine(info);
+    }
+    Console.WriteLine("************************************\n");
+}
+
+static void EnumModules()
+{
+    Process process = Process.GetCurrentProcess();
+    EnumModulesForProcess(process);
+}
+//EnumModules();
+
+static void StartAndKillProcess()
+{
+    Process proc = null;
+    // Launch Microsoft Edge
+    try
+    {
+        //proc = Process.Start(@"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe", "www.weather.com");
+
+        ProcessStartInfo startInfo = new ProcessStartInfo("MsEdge", "www.weather.com");
+        startInfo.UseShellExecute = true;
+        proc = Process.Start(startInfo);
+    }
+    catch (InvalidOperationException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    Console.Write($"--> Hit enter to kill {proc.ProcessName}...");
+    Console.ReadLine();
+
+    try
+    {
+        foreach (var p in Process.GetProcessesByName("MsEdge"))
+        {
+            p.Kill(true);
+        }
+    }
+    catch (InvalidOperationException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+StartAndKillProcess();
+
+void UseApplicationVerbs()
+{
+    ProcessStartInfo processStartInfo = new(@"D:\TheLetter.txt");
+
+    int i = 0;
+    foreach (string? verb in processStartInfo.Verbs)
+    {
+        Console.WriteLine($"  {i++}. {verb}");
+    }
+
+    processStartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+    processStartInfo.Verb = "open";
+    processStartInfo.UseShellExecute = true;
+    Process.Start(processStartInfo);
+}
+UseApplicationVerbs();
