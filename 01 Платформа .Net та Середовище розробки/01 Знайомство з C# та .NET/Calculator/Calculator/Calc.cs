e62e0f8bf7fba1c86b2e8de8d3bf1438f1861b01
AlexFromKiyv <bisoftkievua@gmail.com>
2025-12-08 11:29:07 +0200

    Chapter 01 01

---
+++ 01 Платформа .Net та Середовище розробки/01 Знайомство з C# та .NET/Calculator/Calculator/Calc.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator
+{
+    class Calc
+    {
+        public int Add(int addend1, int addend2)
+        {
+            return addend1 + addend2;
+        }
+    }
+}
---
+++ 01 Платформа .Net та Середовище розробки/01 Знайомство з C# та .NET/Calculator/Calculator/Program.cs
@@ -0,0 +1,7 @@
+﻿using Calculator;
+
+Calc c = new Calc();
+int ans = c.Add(10, 84);
+Console.WriteLine("10 + 84 is {0}.", ans);
+//Wait for user to press the Enter key
+Console.ReadLine();
